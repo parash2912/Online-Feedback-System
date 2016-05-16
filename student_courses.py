76c93867184677b4ad0e94420db1e246cd3014fa
parash2912 <jain.parash@yahoo.in>
2016-05-15 21:08:47 -0400

    Checked if course is not offered in current sem, do not show in available feedbacks

--- student_courses.py
+++ student_courses.py
@@ -5,12 +5,15 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 from student_feedback import StudentSubmitted
+from faculty_courses import CourseTimings
 
 JINJA_ENVIRONMENT = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
 
+current_sem='F2016'
+	
 class CoursesEnrolled(ndb.Model):
 	email=ndb.StringProperty(indexed=True, required=True)
 	courses_enrolled=ndb.StringProperty()
@@ -20,16 +23,25 @@ class StudentHome(webapp2.RequestHandler):
 		user_email=self.request.get('user_email')
 		course_query = CoursesEnrolled.query(CoursesEnrolled.email == user_email)
 		courses_fetched = course_query.fetch()
+		courses_final=[]
 		if len(courses_fetched) == 0:
-			courses=[]
+			courses_final=[]
 		else:
+			courses_final=[]
 			courses = courses_fetched[0].courses_enrolled.split(" ")
+			for course in courses:
+				course_timings_query = CourseTimings.query(CourseTimings.course_year==current_sem)
+				course_timings=course_timings_query.fetch()
+				for course_timing in course_timings:
+					if course_timing.course_id==course:
+						courses_final.append(course)
+						
 		feedback_query = StudentSubmitted.query(StudentSubmitted.email == user_email)
 		feedback_submitted = feedback_query.fetch()
 
 		template_values={
 			'user_email': user_email,
-			'courses': courses,
+			'courses': courses_final,
 			'feedbacks': feedback_submitted
 		}
 
